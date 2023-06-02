00dc97a31a9e2b513ed58542b9f442e5c3b76979
samuelhoover <hooversamuelc@gmail.com>
2023-06-02 10:11:57 -0400

    updated spin-decomp.py

--- spin-decomp.py
+++ spin-decomp.py
@@ -84,8 +84,6 @@ class CahnHilliard:
                 os.remove(os.path.join('figs', f))
 
 
-<<<<<<< HEAD
-
 def main():
     ch = CahnHilliard(
         N=200, p=0.4, D=100, gamma=0.5, 
@@ -93,73 +91,4 @@ def main():
     ) 
     ch.solve()
     ch.animate()
-=======
-def plot_lattice(c, ax, t, i):
-    im = ax.imshow(c)
-    ax.set_xticks([], [])
-    ax.set_yticks([], [])
-    ax.annotate(
-        f'$t = {t:.3f}$', xy=(0.05, 0.95), xycoords='axes fraction',
-        bbox={'color': 'white', 'alpha': 0.8,
-              'boxstyle': 'Round', 'edgecolor': None},
-        ha='left', va='top', fontsize=14
-    )
-    plt.savefig(f'figs/{i:06d}.png')
- 
 
-def forward_euler(x, func, dt, *fargs):
-    return x + func(*fargs) * dt
-
-
-def cahn_hilliard(c, D, gamma):
-    x = c ** 3
-    y = c
-    z = gamma * laplace(c, mode='wrap')
-    return D * laplace(x - y - z, mode='wrap')
-
-
-def solve(c, num_iter, D, gamma, dt, frame_iter):
-    fig, ax = plt.subplots()
-    t = 0
-
-    plot_lattice(c, ax, t, 0)
-    for i in trange(1, num_iter + 1):
-        fargs = (c, D, gamma)
-        ax.clear()
-        c = forward_euler(c, cahn_hilliard, dt, *fargs)
-        t = np.round(t + dt, 6)
-        if i % frame_iter == 0:
-            plot_lattice(c, ax, t, i)
-    return c
-
-
-def main(N=100, p=0, num_iter=1000, D=1, gamma=0.5, dt=0.001, frame_iter=10):
-    c = initialize_lattice(N, p)
-    solve(c, num_iter, D, gamma, dt, frame_iter)
-    
-    # generate gif from snapshots
-    path_in = 'figs/*.png'
-    path_out = f'figs/spinodal-decomposition_D-{D}_gamma-{gamma}_p-{p}.gif'
-    imgs = []
-    for f in sorted(glob.glob(path_in)):
-        img = Image.open(f)
-        imgs.append(img.copy())
-        img.close()
-    imgs[0].save(
-        fp=path_out, format='GIF', append_images=imgs[1:],
-        save_all=True, duration=1, loop=0,
-    )
->>>>>>> master
-
-    # delete snapshots, keep .gif
-    for f in os.listdir('figs'):
-        if f.endswith('.png'):
-            os.remove(os.path.join('figs', f))
-
-
-if __name__ == "__main__":
-<<<<<<< HEAD
-    main()
-=======
-    main(N=200, p=0.4, D=100, gamma=0.5, num_iter=50000, dt=0.0001, frame_iter=100)
->>>>>>> master
