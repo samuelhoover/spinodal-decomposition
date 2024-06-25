7238de6db4fc1754b9c3cbadfec46e220ee8bbed
Samuel Hoover <hooversamuelc@gmail.com>
2024-06-25 19:02:10 -0400

    Reformatted, added typing, and better handling of snapshots

--- spin-decomp.py
+++ spin-decomp.py
@@ -1,94 +1,150 @@
+# pyright: basic
+
 import glob
+import shutil
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 
 from PIL import Image
 from scipy.ndimage import laplace
-from tqdm import trange
+
+SEED: int = 42  # change this for different outcomes
 
 
 class CahnHilliard:
-    def __init__(self, N=100, p=0, num_iter=5000, D=1, gamma=0.5, dt=0.001, frame_iter=10):
+    def __init__(
+        self,
+        N: int = 100,
+        p: float = 0,
+        D: float = 1,
+        gamma: float = 0.5,
+        num_iter: int = 5000,
+        dt: float = 0.001,
+        frame_iter: int = 10,
+    ) -> None:
         self.N = N
         self.p = p
-        self.num_iter = num_iter
         self.D = D
         self.gamma = gamma
+        self.num_iter = num_iter
         self.dt = dt
         self.frame_iter = frame_iter
 
     # Cahn-Hilliard equation
-    def CH_eqn(self,c, D, gamma):
-        return D * laplace((c ** 3) - c - (gamma * laplace(c, mode='wrap')), mode='wrap')
+    def CH_eqn(
+        self, c: npt.NDArray[np.float64], D: float, gamma: float
+    ) -> npt.NDArray[np.float64]:
+        return D * laplace(
+            input=(c**3) - c - (gamma * laplace(input=c, mode="wrap")), mode="wrap"
+        )
 
     # Initialize random data on a 2d square lattice
-    def initialize_lattice(self):
-        rng = np.random.default_rng(seed=42)
-        return rng.choice([-1, 1], (self.N, self.N), 
-                          p=[0.5 + (self.p / 2), 0.5 - (self.p / 2)])
+    def initialize_lattice(self) -> npt.NDArray[np.float64]:
+        rng = np.random.default_rng(seed=SEED)
+        return rng.choice(
+            [-1, 1], (self.N, self.N), p=[0.5 + (self.p / 2), 0.5 - (self.p / 2)]
+        )
 
     # forward Euler method for solving ODEs
-    def forward_euler(self, x, func, dt, *fargs):
+    def forward_euler(self, x, func, dt, *fargs) -> npt.NDArray[np.float64]:
         return x + func(*fargs) * dt
 
     # solver function
-    def solve(self):
-        c = self.initialize_lattice()
+    def solve(self) -> npt.NDArray[np.float64]:
+        c: npt.NDArray[np.float64] = self.initialize_lattice()
         t = 0
 
-        fig, ax = plt.subplots() 
-        self.plot_lattice(c, ax, t, 0)
-        for i in trange(1, self.num_iter + 1):
-            fargs = (c, self.D, self.gamma)
+        _, ax = plt.subplots()
+        self.plot_lattice(c, ax, t, i=0)
+        for i in range(1, self.num_iter + 1):
+            fargs: tuple[npt.NDArray[np.float64], float, float] = (
+                c,
+                self.D,
+                self.gamma,
+            )
             ax.clear()
-            c = self.forward_euler(c, self.CH_eqn, self.dt, *fargs)
+            c: npt.NDArray[np.float64] = self.forward_euler(
+                c, self.CH_eqn, self.dt, *fargs
+            )
             t = np.round(t + self.dt, 6)
             if i % self.frame_iter == 0:
                 self.plot_lattice(c, ax, t, i)
         return c
 
     # create snapshot at time t
-    def plot_lattice(self, c, ax, t, i):
-        im = ax.imshow(c)
+    def plot_lattice(self, c, ax, t, i) -> None:
+        ax.imshow(c)
         ax.set_xticks([], [])
         ax.set_yticks([], [])
         ax.annotate(
-            f'$t = {t:.3f}$', xy=(0.05, 0.95), xycoords='axes fraction',
-            bbox={'color': 'white', 'alpha': 0.8, 
-                  'boxstyle': 'Round', 'edgecolor': None}, 
-            ha='left', va='top', fontsize=14
+            f"$t = {t:.3f}$",
+            xy=(0.05, 0.95),
+            xycoords="axes fraction",
+            bbox={
+                "color": "white",
+                "alpha": 0.8,
+                "boxstyle": "Round",
+                "edgecolor": None,
+            },
+            ha="left",
+            va="top",
+            fontsize=14,
         )
-        plt.tight_layout() 
-        plt.savefig(f'figs/{i:06d}.png')
+        plt.tight_layout()
+
+        # save snapshot
+        if not os.path.exists("gifs/tmp"):
+            os.makedirs("gifs/tmp")
+        plt.savefig(f"gifs/tmp/{i:06d}.png")
 
     # create animation from snapshots
-    def animate(self):
-        # generate gif from snapshots
-        path_in = 'figs/*.png'
-        path_out = f'figs/spinodal-decomposition_D-{self.D}_gamma-{self.gamma}_p-{self.p}.gif'
-        imgs = []
+    def animate(self) -> None:
+        path_in: str = "gifs/tmp/*.png"
+        path_out: str = f"gifs/spin-decomp-D_{self.D}-gamma_{self.gamma}-p_{self.p}.gif"
+        imgs: list[Image.Image] = []
+
+        # grab all snapshots
         for f in sorted(glob.glob(path_in)):
-            img = Image.open(f)
+            img: Image.Image = Image.open(f)
             imgs.append(img.copy())
             img.close()
+
+        # convert snapshots to GIF
         imgs[0].save(
-            fp=path_out, format='GIF', append_images=imgs[1:],
-            save_all=True, duration=1, loop=0,
+            fp=path_out,
+            format="GIF",
+            append_images=imgs[1:],
+            save_all=True,
+            optimize=True,
+            duration=1,
+            loop=0,
         )
 
-        # delete snapshots, keep animation
-        for f in os.listdir('figs'):
-            if f.endswith('.png'):
-                os.remove(os.path.join('figs', f))
+        # delete snapshots
+        if os.path.exists("gifs/tmp"):
+            shutil.rmtree("gifs/tmp/")
 
 
-def main():
-    ch = CahnHilliard(
-        N=200, p=0.4, D=100, gamma=0.5, 
-        num_iter=50000, dt=0.0001, frame_iter=100
-    ) 
+def main(
+    N: int, p: float, D: float, gamma: float, num_iter: int, dt: float, frame_iter: int
+) -> None:
+    ch: CahnHilliard = CahnHilliard(N, p, D, gamma, num_iter, dt, frame_iter)
     ch.solve()
     ch.animate()
 
+
+if __name__ == "__main__":
+    N: int = 500
+    p: float = 0.2
+    D: float = 200
+    gamma: float = 0.5
+    num_iter: int = 10000
+    dt: float = 0.0002
+    frame_iter: int = 100
+    print("Beginning calculation ...\n")
+    print(f"N: {N}\np: {p}\nD: {D}\ngamma: {gamma}\ndt: {dt}\n")
+    main(N, p, D, gamma, num_iter, dt, frame_iter)
+    print("\nDone!")
